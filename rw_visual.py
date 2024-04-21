7ef12318afae094e7766ee4c081f9529b72f0b94
Ilya Savin <ilja.savin18@yandex.ru>
2024-04-21 11:33:57 +0300

    Программа остается активной, пока ...

--- rw_visual.py
+++ rw_visual.py
@@ -2,7 +2,14 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# новые блуждания строятся до тех пор, пока программа остается активной.
+while True:
+# Построение случайного блуждания и нанесения на диграмму
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
